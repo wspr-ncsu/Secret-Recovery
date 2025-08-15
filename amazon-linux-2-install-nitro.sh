0cbd0ac23c4c27edb22ead7a5a998962d708709f
David L. Adei <lokingdav@gmail.com>
2025-08-15 16:54:00 +0000

    updated

--- amazon-linux-2-install-nitro.sh
+++ amazon-linux-2-install-nitro.sh
@@ -8,9 +8,15 @@ sudo systemctl enable docker
 
 sudo usermod -aG docker $USER
 
+sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) -o /usr/libexec/docker/cli-plugins/docker-compose
+sudo chmod +x /usr/libexec/docker/cli-plugins/docker-compose
+
 newgrp docker
 
 sudo dnf install -y aws-nitro-enclaves-cli aws-nitro-enclaves-cli-devel jq
+
+sudo usermod -aG ne $(whoami)
+newgrp ne
 
 # Check the installation
 nitro-cli --version
